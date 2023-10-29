df6750a5f26c240c0656bd64840063423442789b
zhangjingqi <zhangj.q-s@inspur.com>
2023-10-29 23:50:06 +0800

    内容管理模块 - 查询课程Controller开发

---
+++ Code/xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -0,0 +1,32 @@
+package com.xuecheng.base.model;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.java.Log;
+
+/**
+ * @description 分页查询通用参数
+ * @author Mr.M
+ * @date 2022/9/6 14:02
+ * @version 1.0
+ */
+@Data
+@ToString
+public class PageParams {
+
+  //当前页码
+  private Long pageNo = 1L;
+
+  //每页记录数默认值
+  private Long pageSize =10L;
+
+  public PageParams(){
+
+  }
+
+  public PageParams(long pageNo,long pageSize){
+      this.pageNo = pageNo;
+      this.pageSize = pageSize;
+  }
+
+}
