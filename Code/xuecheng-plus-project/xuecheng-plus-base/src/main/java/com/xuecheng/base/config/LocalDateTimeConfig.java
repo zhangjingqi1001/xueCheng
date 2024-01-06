e13d33cf55570802510fb23bc83b7ce21d737307
zhangjingqi <zhangj.q-s@inspur.com>
2024-01-06 23:46:41 +0800

    媒资管理服务 - xxl-job初介绍

--- Code/xuecheng-plus-project/xuecheng-plus-base/src/main/java/com/xuecheng/base/config/LocalDateTimeConfig.java
+++ Code/xuecheng-plus-project/xuecheng-plus-base/src/main/java/com/xuecheng/base/config/LocalDateTimeConfig.java
@@ -1,4 +1,4 @@
-package com.xuecheng.base.utils;
+package com.xuecheng.base.config;
 
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
